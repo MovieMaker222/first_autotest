fac8282b6c294faf4ed26bad7f8da49d6cb67610
MovieMaker222 <iluag228@gmail.com>
2023-09-24 15:46:14 +0300

    add_two_accounts

--- pages/of_pages_names.js
+++ pages/of_pages_names.js
@@ -19,7 +19,8 @@ module.exports={
     loginContinueButton : '//button[@type="button" and @class="v-btn v-btn--is-elevated v-btn--has-bg theme--dark v-size--default primary"]',
     signInButton : '//*[@data-attr="sign-in"]',
     errorLoginMessage : '//div[@class="info-message invalid py-0 px-3"]',
-    errorFailedMessage: 'Перевірте формат email, повинно бути щось типу: user@domain.com' || 'Проверьте формат email, должно быть что-то типа: user@domain.com' || 'Check the email format, it should be something like: user@domain.com',
-    errorRussianMessage: 'У нас заборонені російські поштові сервіси, тому що їх використання загрожує безпеці даних клієнтів. Будь ласка, введіть іншу електронну пошту для реєстрації' || 'У нас запрещены российские почтовые сервисы, потому что их использование угрожает безопасности данных клиентов. Пожалуйста, введите другую электронную почту для регистрации' || 'We have blocked Russian email services because they threaten the security of customer data. Please enter a different email address to sign up',
-
+    errorFailedMessage : 'Перевірте формат email, повинно бути щось типу: user@domain.com' || 'Проверьте формат email, должно быть что-то типа: user@domain.com' || 'Check the email format, it should be something like: user@domain.com',
+    errorRussianMessage : 'У нас заборонені російські поштові сервіси, тому що їх використання загрожує безпеці даних клієнтів. Будь ласка, введіть іншу електронну пошту для реєстрації' || 'У нас запрещены российские почтовые сервисы, потому что их использование угрожает безопасности данных клиентов. Пожалуйста, введите другую электронную почту для регистрации' || 'We have blocked Russian email services because they threaten the security of customer data. Please enter a different email address to sign up',
+    priceProductField : '//*[@data-attr="price-payment-period"]',
+    priceCartField :  '//span[@data-attr-price and @class="price"]',
 }
---
+++ tests/price_check.js
@@ -0,0 +1,12 @@
+const orderformSteps =require('../steps/of_steps');
+const orderformPages = require("../pages/of_pages_names");
+const orderFormPage = require("../pages/of_pages_names");
+
+xScenario('price check on new OF', ({I })=> {
+
+    orderformSteps.openPage(orderformPages.stPage)
+
+
+
+
+})
